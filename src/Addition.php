b02497f13bb3abce02948429915f32f3e2332479
Hichxm <hicham.slimani.fr@gmail.com>
2018-10-03 22:49:40 +0200

    doc: add php documentation to classes

--- src/Addition.php
+++ src/Addition.php
@@ -1,10 +1,23 @@
 <?php
+
 namespace EasyMath;
 
+/**
+ * Class Addition
+ * @package EasyMath
+ */
 class Addition
 {
-    public function add($x, $y)
+
+    /**
+     * Addition two numbers
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function add(float $x, float $y): float
     {
         return $x + $y;
     }
+
 }
--- src/Subtraction.php
+++ src/Subtraction.php
@@ -1,10 +1,23 @@
 <?php
+
 namespace EasyMath;
 
+/**
+ * Class Subtraction
+ * @package EasyMath
+ */
 class Subtraction
 {
-    public function subtract($x, $y)
+
+    /**
+     * Subtract two numbers
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function subtract($x, $y): float
     {
         return $x - $y;
     }
+
 }
